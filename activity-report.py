10ae2376b1689a4b03de540f0d4326bc5eb4a229
Alfonso E.M. <alfem@users.noreply.github.com>
2010-10-05 13:44:57 +0000

    Se incluye el tiempo de sesión en el informe

--- activity-report.py
+++ activity-report.py
@@ -13,19 +13,24 @@ import datetime, time
 homedir = os.path.expanduser('~')
 LOGFILE=homedir+"/activity.log"
 
+def seconds2hms(s):
+  return str(datetime.timedelta(seconds=int(s)))
+
 
 try:
   logfile = open(LOGFILE, "r")
 except IOError:
   print "No es posible leer el registro de actividad "+LOGFILE
 
-seconds=0
+session_seconds=0
+worked_seconds=0
 last_date=""
 last_time=0
 
 days=[]
 
 
+#Recorriendo el fichero de log
 for line in logfile:
   (e_date,e_time,e_type,e_status,e_text)=line.split(",",4)
   struct_time=time.strptime(e_date+","+e_time,"%Y-%m-%d,%H:%M:%S")
@@ -33,21 +38,25 @@ for line in logfile:
 #  print line
 
   if (e_date != last_date):
-    if seconds > 0:
-#      print "TOTAL:",last_date,seconds
-      days.append( [last_date,str(datetime.timedelta(seconds=int(seconds)))] )
-    seconds=0
+    if worked_seconds > 0:
+#      print "TOTAL:",last_date,worked_seconds
+      days.append( [ last_date,seconds2hms(worked_seconds),seconds2hms(session_seconds) ] )
+    session_start_time=0
+    session_seconds=0
+    worked_seconds=0
     last_date=e_date
     last_time=0
 
   if e_type == "START":
     last_time=e_time
-    
+    if session_start_time == 0:
+      session_start_time=e_time
+        
   if e_type == "IDLE" or e_type == "SCREENSAVER":
     if e_status == "1":
       if last_time > 0:
-        seconds=seconds+e_time-last_time
-#        print seconds
+        worked_seconds=worked_seconds+e_time-last_time
+#        print worked_seconds
         last_time=0
     if e_status == "0":
       last_time=e_time
@@ -57,30 +66,34 @@ for line in logfile:
       last_time=e_time
     else:
       if last_time > 0:
-        seconds=seconds+e_time-last_time
-#        print seconds
+        worked_seconds=worked_seconds+e_time-last_time
+#        print worked_seconds
         last_time=0
     
   if e_type == "LOGOUT" and e_status == "1":
       if last_time > 0:
-        seconds=seconds+e_time-last_time
-#        print seconds
+        worked_seconds=worked_seconds+e_time-last_time
+#        print worked_seconds
+      if session_start_time > 0:
+        session_seconds=e_time - session_start_time
 
-
+#Fin del log
 if (last_time > 0 and last_date == str(datetime.date.today())):
-  seconds=seconds+time.time()-last_time
-#  print seconds
+  worked_seconds=worked_seconds+time.time()-last_time
+  session_seconds=e_time - session_start_time
+  
+#  print worked_seconds
  
 
-#print "TOTAL:",last_date,str(datetime.timedelta(seconds=int(seconds)))
-days.append( [last_date,str(datetime.timedelta(seconds=int(seconds)))] )
+#print "TOTAL:",last_date,str(datetime.timedelta(seconds=int(worked_seconds)))
+days.append( [ last_date,seconds2hms(worked_seconds),seconds2hms(session_seconds) ] )
 
 logfile.close()
 
-cmd='zenity --title "Activity" --text "Acumulado diario" --list --column "Fecha" --column "Horas" '
+cmd='zenity --title "Activity" --text "Acumulado diario" --width 400 --height 200 --list --column "Fecha" --column "Tiempo activo" --column "Duración Sesión" '
 
 days.reverse()
 for d in days:
-  cmd=cmd+d[0]+" "+d[1]+" " 
+  cmd=cmd+d[0]+" "+d[1]+" "+d[2]+" " 
 
 p = subprocess.Popen(cmd, shell=True)
